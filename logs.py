fccdf6e08aa5323ccdb322c8a4aff7e2479421af
Carlos Eduardo Ignacio <carlos.ignacio@pravaler.com.br>
2024-04-08 08:59:50 -0300

    Logs.py

--- logs.py
+++ logs.py
@@ -3,19 +3,28 @@
 """
 import os
 import logging
+from logging import handlers
 
 # BOILERPLATE
 # TODO: usar função
 # TODO: usar lib (loguru)
 log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
 log = logging.Logger("logs.py", log_level)
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
+#ch = logging.StreamHandler()
+#ch.setLevel(logging.DEBUG)
+fh = handlers.RotatingFileHandler(
+    "meulog.log", 
+    maxBytes=100,   # 10**6 (1MB)
+    backupCount=10,  # bytes, qtd_files_backup 
+)
+fh.setFormatter(log_level) 
 fmt = logging.Formatter(
     '%(asctime)s %(name)s %(levelname)s l:%(lineno)d f:%(filename)s %(message)s'
 )
-ch.setFormatter(fmt)
-log.addHandler(ch)
+#ch.setFormatter(fmt)
+fh.setFormatter(fmt)
+#log.addHandler(ch)
+log.addHandler(fh)
 
 """
 log.critical("Erro geral ex: banco de dados sumiu")
@@ -28,6 +37,6 @@ log.info("Mensagem geral para usuarios")
 try:
     1 / 0
 except ZeroDivisionError as e:
-    logging.error("Deu erro %s", str(e))
+    log.error("Deu erro %s", str(e))
     # stdout
     # sterr
